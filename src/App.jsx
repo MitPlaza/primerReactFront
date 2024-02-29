0494a7581d09443dfea88253990e0895a305dc80
MItPlaza <m.plazaweb@gmail.com>
2024-02-28 22:14:09 -0300

    Modificado archivo app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     <div className="container">
     <div className="row encajonar">
     <div className='col-md-3'>
-   <MyCard img='https://img.freepik.com/foto-gratis/perro-pug-aislado-fondo-blanco_2829-11409.jpg' titulo='Rocco' descripcion='Un perro de tamaño mediano con un corazón gigante. Hazte amigo de Rocco y experimenta un amor incondicional y muchos pelos!' bg='danger' etiqueta='Pug' />
+   <MyCard img='https://img.freepik.com/foto-gratis/perro-pug-aislado-fondo-blanco_2829-11409.jpg' titulo='Rocco' descripcion='Un perro de tamaño pequeño con un corazón gigante. Hazte amigo de Rocco y experimenta un amor incondicional y muchos pelos!' bg='danger' etiqueta='Pug' />
    </div>
    <div className='col-md-3'>
    <MyCard img='https://img.freepik.com/fotos-premium/pastor-aleman_87557-26781.jpg' titulo='Bartolo' descripcion='Lleno de energía y listo para jugar. !Dale a Bartolo el hogar amoroso que se merece!' bg='success' etiqueta='Pastor Aleman' />
@@ -23,7 +23,6 @@ const App = () => {
    <div className='col-md-3'>
    <MyCard img='https://img.freepik.com/fotos-premium/perro-sentado-hierba-campo_1048944-9293995.jpg' titulo='Princesa' descripcion='Es una compañera leal y cariñosa que adora los mimos y los abrazos. !Ayuda a Princesa a encontrar a su final feliz!' bg='warning' etiqueta='Dachshund' />
    </div>
-  
    </div>
    </div>
    <Footer />
